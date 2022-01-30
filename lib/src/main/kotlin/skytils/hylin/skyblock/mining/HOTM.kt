78ebdf9273ed1e8da6e8f544cf7fea5d931ab82d
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2022-01-29 21:15:24 -0500

    literally every single hotm perk

--- lib/src/main/kotlin/skytils/hylin/skyblock/mining/HOTM.kt
+++ lib/src/main/kotlin/skytils/hylin/skyblock/mining/HOTM.kt
@@ -20,6 +20,7 @@ package skytils.hylin.skyblock.mining
 
 import com.google.gson.JsonObject
 import skytils.hylin.extension.converter.*
+import skytils.hylin.extension.getString
 
 class HOTM(json: JsonObject) {
     val tokens by json.byInt()
@@ -32,12 +33,62 @@ class HOTM(json: JsonObject) {
     val gemstonePowder by json.byLong("powder_gemstone")
     val lastHOTMReset by json.byDate("last_reset")
     val lastCrystalHollowsAccess by json.byDate("greater_mines_last_access")
-    val selectedPickaxeAbility by json.byString("selected_pickaxe_ability")
+    val selectedPickaxeAbility = HOTMSlot.slots.find { it.id == json.getString("selected_pickaxe_ability") }
     private val nodes: Map<String, Any> by json.byMap("nodes")
-    val hotmLevel = nodes["special_0"] as Int
-    val toggledPerks = nodes.filter { it.key.startsWith("toggle_") && it.value is Boolean }.entries.associate {
-        it.key.substringAfter("toggle_") to it.value as Boolean
+    val toggledPerks = nodes.filter { it.key.startsWith("toggle_") && it.value is Boolean }.entries.associate { entry ->
+        HOTMSlot.slots.find { entry.key.substringAfter("toggle_") == it.id }!! to entry.value as Boolean
     }
-    @Suppress("unchecked_cast")
-    val perks = nodes.filter { it.value is Int && it.value in 0..50 && !it.key.startsWith("special_") && !it.key.startsWith("toggle_") } as Map<String, Int>
+    val perks = nodes.filter { it.value is Int && !it.key.startsWith("toggle_") }.entries.associate { entry ->
+        HOTMSlot.slots.find { entry.key == it.id }!! to entry.value as Int
+    }
+
+
+    sealed class HOTMSlot(val id: String, val name: String, val slotNum: Int) {
+        sealed class Perk(id: String, name: String, maxLevel: Int, slotNum: Int) : HOTMSlot(id, name, slotNum) {
+            sealed class SpecialPerk(id: String, name: String, maxLevel: Int, slotNum: Int) : Perk(id, name, maxLevel, slotNum) {
+                object PeakOfTheMountain : SpecialPerk("special_0", "Peak of the Mountain", 5, 22)
+            }
+            object MiningSpeed2 : Perk("mining_speed_2", "Mining Speed II", 50, 2)
+            object PowderBuff : Perk("powder_buff", "Powder Buff", 50, 4)
+            object MiningFortune2 : Perk("mining_fortune_2", "Mining Fortune II", 50, 6)
+            object LonesomeMiner : Perk("lonesome_miner", "Lonesome Miner", 45, 11)
+            object Professional : Perk("professional", "Professional", 140, 12)
+            object Mole : Perk("mole", "Mole", 190, 13)
+            object Fortunate : Perk("fortunate", "Fortunate", 20, 14)
+            object GreatExplorer : Perk("great_explorer", "Great Explorer", 20, 15)
+            object GoblinKiller : Perk("goblin_killer", "Goblin Killer", 1, 20)
+            object StarPowder : Perk("star_powder", "Star Powder", 1, 24)
+            object SkyMall : Perk("daily_effect", "Sky Mall", 1, 28)
+            object MiningMadness : Perk("mining_madness", "Mining Madness", 1, 29)
+            object SeasonedMineman : Perk("mining_experience", "Seasoned Mineman", 100, 30)
+            object EfficientMiner : Perk("efficient_miner", "Efficient Miner", 100, 31)
+            object Orbiter : Perk("experience_orbs", "Orbiter", 80, 32)
+            object FrontLoaded : Perk("front_loaded", "Front Loaded", 1, 33)
+            object PrecisionMining : Perk("precision_mining", "Precision Mining", 1, 34)
+            object LuckOfTheCave : Perk("random_event", "Luck of the Cave", 45, 38)
+            object DailyPowder : Perk("daily_powder", "Daily Powder", 100, 40)
+            object Crystallized : Perk("fallen_star_bonus", "Crystallized", 30, 42)
+            object TitaniumInsanium : Perk("titanium_insanium", "Titanium Insanium", 50, 48)
+            object MiningFortune : Perk("mining_fortune", "Mining Fortune", 50, 49)
+            object QuickForge : Perk("forge_time", "Quick Forge", 20, 50)
+            object MiningSpeed : Perk("mining_speed", "Mining Speed", 50, 58)
+        }
+        sealed class PickaxeAbility(id: String, name: String, slotNum: Int) : HOTMSlot(id, name, slotNum) {
+            object VeinSeeker : PickaxeAbility("vein_seeker", "Vein Seeker",  10)
+            object ManiacMiner : PickaxeAbility("maniac_miner", "Maniac Miner", 16)
+            object MiningSpeedBoost : PickaxeAbility("mining_speed_boost", "Mining Speed Boost", 47)
+            object Pickobulus : PickaxeAbility("pickaxe_toss", "Pickobulus", 51)
+        }
+
+        companion object {
+            val slots = hashSetOf<HOTMSlot>()
+
+            init {
+                HOTMSlot::class.sealedSubclasses.flatMapTo(slots) { type ->
+                    type.sealedSubclasses.mapNotNull { item -> item.objectInstance }
+                }
+            }
+        }
+    }
+
 }
