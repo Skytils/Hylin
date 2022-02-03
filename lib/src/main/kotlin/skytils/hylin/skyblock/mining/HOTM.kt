0a4100c0d5a5fbc1bf9ec8e675bce085e9e1ebae
Sychic <47618543+Sychic@users.noreply.github.com>
2022-02-03 15:09:10 -0500

    Add newlines

--- lib/src/main/kotlin/skytils/hylin/skyblock/mining/HOTM.kt
+++ lib/src/main/kotlin/skytils/hylin/skyblock/mining/HOTM.kt
@@ -77,19 +77,23 @@ class HOTM(json: JsonObject) {
                 }
             }
             object MiningSpeed2 : Perk("mining_speed_2", "Mining Speed II", 50, 2) {
-                override fun getLore(level: Int) = listOf("§7Grants §a+${40 * level} §6⸕ Mining Speed§7.")
+                override fun getLore(level: Int) = listOf("§7Grants §a+${40 * level} §6⸕ Mining", "§6Speed§7.")
             }
             object PowderBuff : Perk("powder_buff", "Powder Buff", 50, 4) {
-                override fun getLore(level: Int) = listOf("§7Gain §a${level}% §7more Mithril Powder and Gemstone Powder.")
+                override fun getLore(level: Int) = listOf("§7Gain §a${level}% §7more Mithril", "§7Powder and Gemstone Powder.")
             }
             object MiningFortune2 : Perk("mining_fortune_2", "Mining Fortune II", 50, 6) {
-                override fun getLore(level: Int) = listOf("§7Grants §a+${5 * level} §6☘ Mining Fortune§7.")
+                override fun getLore(level: Int) = listOf("§7Grants §a+${5 * level} §6☘ Mining", "§6Fortune§7.")
             }
             object LonesomeMiner : Perk("lonesome_miner", "Lonesome Miner", 45, 11) {
-                override fun getLore(level: Int) = listOf("§7Increases §c❁ Strength, §9☣ Crit Chance, §9☠ Crit Damage, §a❈ Defense, and §c❤ Health §7statistics gain by §a${round(5 + (level - 1) * 0.5)}% §7while in the Crystal Hollows.")
+                override fun getLore(level: Int) = listOf("§7Increases §c❁ Strength, §9☣ Crit",
+                        "§9Chance, §9☠ Crit Damage, §a❈",
+                        "§aDefense, and §c❤ Health",
+                        "§7statistics gain by §a${round(5 + (level - 1) * 0.5)}%",
+                        "§7while in the Crystal Hollows.")
             }
             object Professional : Perk("professional", "Professional", 140, 12) {
-                override fun getLore(level: Int) = listOf("§7Gain §a+${(5 * level) + 50}§7 §6⸕ Mining Speed§7 when mining Gemstones.")
+                override fun getLore(level: Int) = listOf("§7Gain §a+${(5 * level) + 50}§7 §6⸕ Mining", "§6Speed§7 when mining Gemstones.")
             }
             object Mole : Perk("mole", "Mole", 190, 13) {
                 override fun getLore(level: Int): List<String> {
@@ -108,23 +112,34 @@ class HOTM(json: JsonObject) {
                         else -> "a ${blocks}th"
                     }
 
-                    return listOf("§7When mining hard stone, you have a §a${percent}%§7 chance to mine §a${blocks}§7 adjacent hard stone block.")
+                    return listOf("§7When mining hard stone, you have",
+                            "§7a §a${percent}%§7 chance to mine §a${blocks}",
+                            "§7adjacent hard stone block.")
                 }
             }
             object Fortunate : Perk("fortunate", "Fortunate", 20, 14) {
-                override fun getLore(level: Int) = listOf("§7Grants §a+${5 * level}§7 §6☘ Mining Fortune§7 when mining Gemstone.")
+                override fun getLore(level: Int) = listOf("§7Grants §a+${5 * level}§7 §6☘ Mining",
+                    "§6Fortune§7 when mining Gemstone.")
             }
             object GreatExplorer : Perk("great_explorer", "Great Explorer", 20, 15) {
-                override fun getLore(level: Int) = listOf("§7Grants §a+${20 + (level - 1) * 4}%§7 §7chance to find treasure.")
+                override fun getLore(level: Int) = listOf("§7Grants §a+${20 + (level - 1) * 4}%§7 §7chance to","§7find treasure.")
             }
             object GoblinKiller : Perk("goblin_killer", "Goblin Killer", 1, 20) {
-                override fun getLore(level: Int) = listOf("§7Killing a §6Golden Goblin §7gives §2200 §7extra §2Mithril Powder§7, while killing other Goblins gives some based on their wits.")
+                override fun getLore(level: Int) = listOf(
+                    "§7Killing a §6Golden Goblin",
+                    "§7gives §2200 §7extra §2Mithril",
+                    "§2Powder§7, while killing other",
+                    "§7Goblins gives some based on",
+                    "§7their wits."
+                )
             }
             object StarPowder : Perk("star_powder", "Star Powder", 1, 24) {
-                override fun getLore(level: Int) = listOf("§7Mining Mithril Ore near §5Fallen Crystals §7gives §a+3 §7extra Mithril Powder.")
+                override fun getLore(level: Int) = listOf("§7Mining Mithril Ore near §5Fallen","§5Crystals §7gives §a+3","§7extra Mithril Powder.")
             }
             object SkyMall : Perk("daily_effect", "Sky Mall", 1, 28) {
-                override fun getLore(level: Int) = listOf(      "§7Every SkyBlock day, you receive a random buff in the §2Dwarven Mines§7.",
+                override fun getLore(level: Int) = listOf(
+                    "§7Every SkyBlock day, you receive",
+                    "§7a random buff in the §2Dwarven Mines§7.",
                     "",
                     "§7Possible Buffs",
                     "§8 ■ §7Gain §a+100 §6⸕ Mining Speed.",
@@ -135,46 +150,46 @@ class HOTM(json: JsonObject) {
                     "§8 ■ §7Gain §a5x §9Titanium §7drops.",)
             }
             object MiningMadness : Perk("mining_madness", "Mining Madness", 1, 29) {
-                override fun getLore(level: Int) = listOf("§7Grants §a+50 §6⸕ Mining Speed §7and §6☘ Mining Fortune§7.")
+                override fun getLore(level: Int) = listOf("§7Grants §a+50 §6⸕ Mining Speed", "§7and §6☘ Mining Fortune§7.")
             }
             object SeasonedMineman : Perk("mining_experience", "Seasoned Mineman", 100, 30) {
-                override fun getLore(level: Int) = listOf("§7Increases your Mining experience gain by §a${(5 + level * 0.1).roundToPrecision(1)}%§7.")
+                override fun getLore(level: Int) = listOf("§7Increases your Mining", "§7experience gain by §a${(5 + level * 0.1).roundToPrecision(1)}%§7.")
             }
             object EfficientMiner : Perk("efficient_miner", "Efficient Miner", 100, 31) {
-                override fun getLore(level: Int) = listOf("§7When mining ores, you have a §a${(10 + level * 0.4).roundToPrecision(1)}%§7 chance to mine §a${ceil((level + 1) / 20.0)} §7adjacent ores.")
+                override fun getLore(level: Int) = listOf("§7When mining ores, you have a", "§a${(10 + level * 0.4).roundToPrecision(1)}%§7 chance to mine §a${ceil((level + 1) / 20.0)}", "§7adjacent ores.")
             }
             object Orbiter : Perk("experience_orbs", "Orbiter", 80, 32) {
-                override fun getLore(level: Int) = listOf("§7When mining ores, you have a §a${(0.2 + level * 0.01).roundToPrecision(2)}%§7 chance to get a random amount of experience orbs.")
+                override fun getLore(level: Int) = listOf("§7When mining ores, you have a", "§a${(0.2 + level * 0.01).roundToPrecision(2)}%§7 chance to get a random", "§7amount of experience orbs.")
             }
             object FrontLoaded : Perk("front_loaded", "Front Loaded", 1, 33) {
-                override fun getLore(level: Int) = listOf("§7Grants §a+100 §6⸕ Mining Speed §7and §6☘ Mining Fortune §7for the first §e2,500 §7ores you mine in a day.")
+                override fun getLore(level: Int) = listOf("§7Grants §a+100 §6⸕ Mining Speed", "§7and §6☘ Mining Fortune §7for", "§7the first §e2,500 §7ores you", "§7mine in a day.")
             }
             object PrecisionMining : Perk("precision_mining", "Precision Mining", 1, 34) {
-                override fun getLore(level: Int) = listOf("§7When mining ore, a particle target appears on the block that increases your §6⸕ Mining Speed §7by §a30% §7when aiming at it.")
+                override fun getLore(level: Int) = listOf("§7When mining ore, a particle", "§7target appears on the block that", "§7increases your §6⸕ Mining Speed", "§7by §a30% §7when aiming at it.")
             }
             object LuckOfTheCave : Perk("random_event", "Luck of the Cave", 45, 38) {
-                override fun getLore(level: Int) = listOf("§7Increases the chance for you to trigger rare occurrences in §2Dwarven Mines §7by §a${5 + level}%§7.")
+                override fun getLore(level: Int) = listOf("§7Increases the chance for you to", "§7trigger rare occurrences in", "§2Dwarven Mines §7by §a${5 + level}%§7.")
             }
             object DailyPowder : Perk("daily_powder", "Daily Powder", 100, 40) {
-                override fun getLore(level: Int) = listOf("§7Gain §a${400 + (level - 1) * 36} Powder §7from the first ore you mine every day. Works for all Powder types.")
+                override fun getLore(level: Int) = listOf("§7Gain §a${400 + (level - 1) * 36} Powder §7from the", "§7first ore you mine every day.", "§7Works for all Powder types.")
             }
             object Crystallized : Perk("fallen_star_bonus", "Crystallized", 30, 42) {
                 override fun getLore(level: Int): List<String> {
                     val num = 20 + (level - 1) * 6
-                    return listOf("§7Grants §a+${num} §6⸕ Mining Speed §7and a §a${num}% §7chance to deal §a+1 §7extra damage near §5Fallen Stars§7.")
+                    return listOf("§7Grants §a+${num} §6⸕ Mining", "§6Speed §7and a §a${num}% §7chance to", "§7deal §a+1 §7extra damage near", "§5Fallen Stars§7.")
                 }
             }
             object TitaniumInsanium : Perk("titanium_insanium", "Titanium Insanium", 50, 48) {
-                override fun getLore(level: Int) = listOf("§7When mining Mithril Ore, you have a §a${(2 + level * 0.1).roundToPrecision(1)}%§7 chance to convert the block into Titanium Ore.")
+                override fun getLore(level: Int) = listOf("§7When mining Mithril Ore, you", "§7have a §a${(2 + level * 0.1).roundToPrecision(1)}%§7 chance to", "§7convert the block into Titanium", "§7Ore.")
             }
             object MiningFortune : Perk("mining_fortune", "Mining Fortune", 50, 49) {
-                override fun getLore(level: Int) = listOf("§7Grants §a+${5 * level} §6☘ Mining Fortune§7.")
+                override fun getLore(level: Int) = listOf("§7Grants §a+${5 * level} §6☘ Mining", "§6Fortune§7.")
             }
             object QuickForge : Perk("forge_time", "Quick Forge", 20, 50) {
-                override fun getLore(level: Int): List<String> = listOf("§7Decreases the time it takes to forge by §a${if (level == maxLevel) 30 else (10 + 0.5 * level).roundToPrecision(1)}%§7.")
+                override fun getLore(level: Int): List<String> = listOf("§7Decreases the time it takes to", "§7forge by §a${if (level == maxLevel) 30 else (10 + 0.5 * level).roundToPrecision(1)}%§7.")
             }
             object MiningSpeed : Perk("mining_speed", "Mining Speed", 50, 58) {
-                override fun getLore(level: Int) = listOf("§7Grants §a+${20 * level} §6⸕ Mining Speed§7.")
+                override fun getLore(level: Int) = listOf("§7Grants §a+${20 * level} §6⸕ Mining", "§6Speed§7.")
             }
 
             override fun getItem(level: Int): ItemStack {
@@ -189,27 +204,34 @@ class HOTM(json: JsonObject) {
             object VeinSeeker : PickaxeAbility("vein_seeker", "Vein Seeker",  10) {
                 override fun getLore(level: Int) = listOf(
                     "§6Pickaxe Ability: Vein Seeker",
-                    "§7Points in the direction of the nearest vein and grants §a+${1 + level} §6Mining Spread §7for §a${10 + level * 2}s§7.",
+                    "§7Points in the direction of the nearest vein ",
+                    "and grants §a+${1 + level} §6Mining Spread §7for §a${10 + level * 2}s§7.",
                     "§8Cooldown: §a60s"
                 )
             }
             object ManiacMiner : PickaxeAbility("maniac_miner", "Maniac Miner", 16) {
                 override fun getLore(level: Int) = listOf(
                     "§6Pickaxe Ability: Maniac Miner",
-                    "§7Spends all your Mana and grants §a+[Some] §6⸕ Mining Speed §7for every 10 Mana spent, for §a${5 + level * 5}s§7.",
+                    "§7Spends all your Mana and grants",
+                    "§a+[Some] §6⸕ Mining Speed §7for",
+                    "every 10 Mana spent, for §a${5 + level * 5}s§7.",
                     "§8Cooldown: §a${if (level <= 1) 60 else 59}s"
                 )
             }
             object MiningSpeedBoost : PickaxeAbility("mining_speed_boost", "Mining Speed Boost", 47) {
                 override fun getLore(level: Int) = listOf(
                     "§6Pickaxe Ability: Mining Speed Boost",
-                    "§7Grants §a+${100 + level * 100}% §6⸕ Mining Speed §7for §a${10 + level * 5}s§7."
+                    "§7Grants §a+${100 + level * 100}% §6⸕ Mining",
+                    "§6Speed §7for §a${10 + level * 5}s§7."
                 )
             }
             object Pickobulus : PickaxeAbility("pickaxe_toss", "Pickobulus", 51) {
                 override fun getLore(level: Int) = listOf(
                     "§6Pickaxe Ability: Pickobulus",
-                    "§7Throw your pickaxe to create an explosion on impact, mining all ores within a §a${if (level == 3) 3 else 2}§7 block radius.",
+                    "§7Throw your pickaxe to create an",
+                    "§7explosion on impact, mining all",
+                    "§7ores within a §a${if (level == 3) 3 else 2}§7 block",
+                    "§7radius.",
                     "§8Cooldown: §a${if (level <= 1) 120 else 110}s"
                 )
             }
